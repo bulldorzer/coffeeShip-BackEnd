cf1960f2a7b87171616dfd5d9cff63ee95e4479d
Bulldorzer <buad452@gmail.com>
2025-03-25 15:44:39 +0900

    Order.java 파일추가 - 진우

--- src/main/java/com/teamproject/coffeeShop/domain/Order.java
+++ src/main/java/com/teamproject/coffeeShop/domain/Order.java
@@ -1,4 +1,20 @@
 package com.teamproject.coffeeShop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
 public class Order {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    private LocalDateTime orderDate;
 }
