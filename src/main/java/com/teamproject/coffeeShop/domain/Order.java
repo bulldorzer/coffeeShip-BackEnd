4b63efe23f08cb8b3280af60fa9ea3a3fe00f12c
Bulldorzer <buad452@gmail.com>
2025-03-27 10:48:38 +0900

    OrderCoffeeBean Order 엔티티 수정- 진우

--- src/main/java/com/teamproject/coffeeShop/domain/Order.java
+++ src/main/java/com/teamproject/coffeeShop/domain/Order.java
@@ -18,14 +18,14 @@ public class Order {
     private Long id;
 
     // FK member_id 참조
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "member_id")
-//    private Member member;
-//
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
      // FK delivery_id 참조
-//    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST) // order의 배달주소, 상태가 바뀌면 delivery의 데이터 배달주소나 상태가 동시에 바뀜
-//    @JoinColumn(name = "delivery_id")
-//    private Delivery delivery;
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST) // order의 배달주소, 상태가 바뀌면 delivery의 데이터 배달주소나 상태가 동시에 바뀜
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
 
     // 주문날짜
     private LocalDate orderDate;
@@ -35,10 +35,10 @@ public class Order {
     private OrderStatus status;
 
     // 주문취소시 상태 변경
-//    public void cancel() {
-//        if (delivery.getStatus() == DeliveryStatus.COMP) {
-//            throw new IllegalStateException("이미 배송 완료된 상품은 취소 불가");
-//        }
-//        this.status = OrderStatus.CANCEL;
-//    }
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소 불가");
+        }
+        this.status = OrderStatus.CANCEL;
+    }
 }
