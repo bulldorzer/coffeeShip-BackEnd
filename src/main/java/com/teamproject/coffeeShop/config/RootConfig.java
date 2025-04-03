75377a4bc919ec170387352fc98c08c05d73f52f
Bulldorzer <buad452@gmail.com>
2025-04-03 10:31:04 +0900

    RootConfig modelmapper설정 변경 - 진우 Loose -> Standard

--- src/main/java/com/teamproject/coffeeShop/config/RootConfig.java
+++ src/main/java/com/teamproject/coffeeShop/config/RootConfig.java
@@ -1,5 +1,7 @@
 package com.teamproject.coffeeShop.config;
 
+import com.teamproject.coffeeShop.domain.Order;
+import com.teamproject.coffeeShop.dto.OrderDTO;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +16,19 @@ public class RootConfig {
     modelMapper.getConfiguration()
             .setFieldMatchingEnabled(true)
             .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
-            .setMatchingStrategy(MatchingStrategies.LOOSE);
+            .setMatchingStrategy(MatchingStrategies.STANDARD); // LOOSE -> STANDARD 변경
+
+    /*
+        city, street, zipcode가 member와 delivery에 동시에 존재하고 있어
+        어느 엔티티것을 매핑해야하는지에대한 오류가 발생함으로 수동으로 매핑해줌
+        modelmapper 설정
+    */
+    // 🚀 Order -> OrderDTO 매핑 설정 추가
+    modelMapper.typeMap(Order.class, OrderDTO.class).addMappings(mapper -> {
+      mapper.map(src -> src.getDelivery().getCity(), OrderDTO::setCity);
+      mapper.map(src -> src.getDelivery().getStreet(), OrderDTO::setStreet);
+      mapper.map(src -> src.getDelivery().getZipcode(),OrderDTO::setZipcode);
+    });
 
     return modelMapper;
   }
